a98a4c7318f23a19fbef295c65c0c8c4b15f243d
xuanmenliuyao <1601958350@qq.com>
2024-09-10 17:24:58 +0800

    dev

--- functions/manage/log/desc/[id].js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-    // Contents of context object
-    const { env, params } = context;
-
-    const ps = env.IMG.prepare(`SELECT ${params.id}, COUNT(*) AS count FROM tgimglog GROUP BY ${params.id} ORDER BY count DESC LIMIT 20`)
-    const { results } = await ps.all()
-    return Response.json(results);
-
-}
